2f02f31d3ab972eb7af2bf626ec2dfb1ff4cb348
sion-1122 <sionsaiyan.com>
2024-11-09 10:45:00 -0800

    SIMPLE PORTFOLIO APP IN NEXTJS

---
+++ constants/index.ts
@@ -0,0 +1,57 @@
+// constants/index.ts
+export const personalInfo = {
+  name: "Laiba Ali",
+  title: "Beginner Programmer",
+  description: "I'm a passionate beginner programmer learning web development.",
+  email: "laibaali858452@example.com",
+  github: "https://github.com/laibaali85",
+  linkedin: "https://www.linkedin.com/in/laiba-ali-0763522b4/",
+  twitter: "#",
+};
+
+export const projects = [
+  {
+    title: "To-Do-List-App Nextjs",
+    description: "A To-Do App in Next.js is a web application built using Next.js that allows users to manage tasks with features like adding, editing, and deleting items.",
+    link: "https://github.com/laibaali85/To-Do-App-Nextjs",
+  },
+  {
+    title: "HTML AND CSS responsive Website",
+    description: "HTML AND CSS responsive Website . An HTML and CSS responsive website adapts seamlessly to different screen sizes and devices, ensuring a user-friendly experience across desktops, tablets, and smartphones. ",
+    link: "https://laibaali85.github.io/HTML-CSS-Responsive-Website/#home",
+  },
+  {
+    title: "CLI Based Calculator",
+    description:
+      "I Made a Command Line Based CLI calculator Using Typescript ,Node JS, And Inquirer .A CLI-Based Calculator is a simple, text-based application that allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division directly from the command-line interface (CLI).",
+    link: "https://github.com/laibaali85/simple-CLI-calculator",
+  },
+  {
+    title: "CLI Based Number-Guessing-Game",
+    description:
+      " Made a Command Line Based CLI Number-Guessing-Game Using Typescript ,Node JS, And Inquirer .A CLI-Based Number Guessing Game is a simple, text-based game that runs in the command-line interface (CLI). ",
+    link: "https://github.com/laibaali85/CLI-number-guessing-game",
+  },
+  {
+    title: "CLI Based ATM",
+    description: "A CLI-Based ATM simulates basic banking operations such as deposits, withdrawals, and balance checks through a text-based interface with user authentication. ",
+    link: "https://github.com/laibaali85/project03-ATM",
+  },
+  {
+    title: "CLI Based To-Do-List",
+    description: "Made a Command Line Based CLI To-Do-List Using Typescript ,Node JS, And Inquirer .CLI Based To-Do List: A command-line application to create, update, and manage a personal to-do list. ",
+    link: "https://github.com/laibaali85/project04-todo-list",
+  },
+  {
+    title: "CLI Based Currency-Converter",
+    description: "Made a Command Line Based CLI Currency-Converter Using Typescript ,Node JS, And Inquirer .CLI Based Currency Converter: A text-based tool that converts currencies using live exchange rates. ",
+    link: "https://github.com/laibaali85/project05-currency-converter",
+  },
+  {
+    title: "CLI Based Word-Counter",
+    description: "Made a Command Line Based CLI Word-Counter Using Typescript ,Node JS, And Inquirer .CLI Based Word Counter: A command-line tool that counts words, characters, and sentences in a text input or file. ",
+    link: "https://github.com/laibaali85/project06-word-counter",
+  },
+];
+
+export const skills = ["HTML", "CSS", "JavaScript", "React", "Next.js", "Git"];
